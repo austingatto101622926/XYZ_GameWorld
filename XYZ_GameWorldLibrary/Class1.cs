c67550d964de4be29b28982f253572de4761aba3
Austin Gatto <Austin Gatto@DESKTOP-QQT0EDN>
2018-01-30 01:41:13 +1100

    Add project files.

---
+++ XYZ_GameWorldLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ_GameWorldLibrary
+{
+    public class Class1
+    {
+    }
+}
